37047d2160d931cb135a9750ad4f31de0655a900
jerryhe16 <66060990+jerryhe16@users.noreply.github.com>
2023-09-20 19:48:24 +0000

    Added some penguin themed words for class

---
+++ js/adverbs/penguin.js
@@ -0,0 +1,12 @@
+adverbs = adverbs.concat([
+    'Waddlingly',
+    'Endearingly',
+    'Gracefully',
+    'Comically',
+    'Deliberately',
+    'Awkwardly',
+    'Peculiarly',
+    'Charmingly',
+    'Synchronizedly',
+    'Playfully'
+]);
---
+++ js/verbs/penguin.js
@@ -0,0 +1,18 @@
+verbs = verbs.concat([
+    'play',
+    'tumble',
+    'pick',
+    'fold',
+    'lick',
+    'melt',
+    'camp',
+    'cook',
+    'fill',
+    'hand',
+    'roll',
+    'lift',
+    'chill',
+    'color',
+    'snack',
+    'slide'
+]);
